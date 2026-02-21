c48dc5c1d22234bce38b2f195ce5725f9d42fa02
Ariel Zuniga <ariel.cerda10@inacapmail.cl>
2026-02-21 17:32:14 -0300

    implemente inicio de sesion , generacion de jwt y corregi la ruta de el login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-import {createUser} from "../models/user.model.js"
+import {createUser,getUserByEmail} from "../models/user.model.js"
 
 export const registerUser = async (req,res) => {
     try{
@@ -27,4 +28,45 @@ export const registerUser = async (req,res) => {
 
     }
    
+}
+
+export const loginUser = async(req,res) =>{
+    try{
+        const {email,password} = req.body
+
+        const user = await getUserByEmail(email)
+
+        if (!user){
+            return res.status(401).json({message: "Credenciales inválidas"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid){
+            return res.status(401).json({message: "Credenciales inválidas"})
+        }
+
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"}
+        )
+
+        res.status(200).json({
+            message: "Inicio de sesión exitoso",
+            token: token,
+            user:{
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }
+
+
+        })
+
+    }catch(error){
+        console.log("Error en el login:", error)
+        res.status(500).json({message: "Error interno del servidor"})
+
+    }
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/auth.controller.js";
+import {registerUser,loginUser} from "../controllers/auth.controller.js";
 
 const router  = Router()
 
 router.post("/register", registerUser)
+router.post('/login', loginUser);
 
 export default router
